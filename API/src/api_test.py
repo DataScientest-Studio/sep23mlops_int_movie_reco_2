39ed47e6a82a3282244754e8b475f45d56a98404
grsantosv <grsantosv@gmail.com>
2024-02-11 13:12:07 -0300

    test

--- API/src/api_test.py
+++ API/src/api_test.py
@@ -1,20 +1,13 @@
 from fastapi.testclient import TestClient
 from main import app
-from requests.auth import HTTPBasicAuth
-import warnings
-warnings.filterwarnings("ignore")
-
+#from requests.auth import HTTPBasicAuth
+#import pytest
+#import warnings
+#warnings.filterwarnings("ignore")
 
 
 client = TestClient(app)
 
-basic = HTTPBasicAuth(11, 'senha123')
-
-
 def health_root():
     response = client.get("/")
-    assert response.status_code == 200
-    
-def User_pred_root():
-    response = client.get("/movie_reco_via_user",auth=basic)
     assert response.status_code == 200
